b9b64bc5f71f4a030c181fb4614733ac6f662803
basiacarvalho <basiacarvalho@gmail.com>
2025-01-02 09:25:22 +0100

    removing comments from the script.js file

--- script.js
+++ script.js
@@ -35,36 +35,26 @@ initialSettings();
 const switchPlayer = function () {
   document.getElementById(`round-score-${activePlayer}`).textContent = 0;
   roundScore = 0;
-  //Showing question dice agan, when changing players
   diceElement.src = "./images/dice-random.png";
-  //Switching players
   activePlayer = activePlayer === 1 ? 2 : 1;
   player1SectionElement.classList.toggle("player-active");
   player2SectionElement.classList.toggle("player-active");
 };
 
-//Rolling dice functionality
 buttonRoll.addEventListener("click", async function () {
   if (playingGame) {
-    //Hiding a random dice picture
     diceElement.classList.add("hidden");
 
-    //1. Generating a random dice roll
     const diceNumber = Math.trunc(Math.random() * 6) + 1;
-    console.log(diceNumber);
 
-    //2. Display the dice
     diceElement.classList.remove("hidden");
     diceElement.src = `./images/dice-${diceNumber}.png`;
 
-    //3. Check for rolled 1: if true, switch to next player
     if (diceNumber !== 1) {
-      //Add dice to a round score
       roundScore += diceNumber;
       document.getElementById(`round-score-${activePlayer}`).textContent =
         roundScore;
     } else {
-      //Switch to a next player
       await new Promise((r) => setTimeout(r, 1000));
       switchPlayer();
     }
@@ -73,22 +63,17 @@ buttonRoll.addEventListener("click", async function () {
 
 buttonHold.addEventListener("click", function () {
   if (playingGame) {
-    //1. Add current score to active player's total score
     if (activePlayer === 1) {
       scores[0] += roundScore;
       document.getElementById(`total-score-${activePlayer}`).textContent =
         scores[0];
-      //Checking if scores are 100 or more
-      //If "Yes" then we have a winner
       if (scores[0] >= 100) {
         playingGame = false;
-        //Showing question dice agan, when finishing game
         diceElement.src = "./images/dice-random.png";
         player1SectionElement.classList.toggle("player-active");
         document.querySelector(".winner-text").textContent =
           "Player 1 is the winner!";
         document.querySelector(".winner-section").classList.remove("hidden");
-        //If "NOT" then switch players
       } else {
         switchPlayer();
       }
@@ -98,7 +83,6 @@ buttonHold.addEventListener("click", function () {
         scores[1];
       if (scores[1] >= 100) {
         playingGame = false;
-        //Showing question dice agan, when finishing game
         diceElement.src = "./images/dice-random.png";
         player2SectionElement.classList.toggle("player-active");
         document.querySelector(".winner-text").textContent =
@@ -111,5 +95,4 @@ buttonHold.addEventListener("click", function () {
   }
 });
 
-//Resetting game
 buttonNew.addEventListener("click", initialSettings);
